f9fdf1076fed0ab4c6eca36b244baa833d40cf17
Rayman Zhang <raymanzhang@gmail.com>
2012-11-22 16:48:09 +0800

    merge code from Alex Lee

---
+++ src/cn/mdict/widgets/SearchTrack.java
@@ -0,0 +1,23 @@
+package cn.mdict.widgets;
+
+public class SearchTrack {
+    private int scrollX;
+    private int scrollY;
+
+    public int getScrollX() {
+        return scrollX;
+    }
+
+    public void setScrollX(int value) {
+        scrollX = value;
+    }
+
+
+    public int getScrollY() {
+        return scrollY;
+    }
+
+    public void setScrollY(int value) {
+        scrollY = value;
+    }
+}
